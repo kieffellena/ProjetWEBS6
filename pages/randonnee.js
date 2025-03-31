07f2b49eaa16ab558c6482e29deea745d45edb48
lenakieffel <kieffellena@gmail.com>
2025-03-31 19:08:33 +0200

    add server + palette + les squelettes pages

---
+++ pages/randonnee.js
@@ -0,0 +1,14 @@
+export function rando() {
+    return `
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <h1>Bonjour ! Je suis la page rando</h1>
+                </head>
+                <body>
+                    <h2>Bienvenue sur ma page.</h2>
+                </body>
+            </html>
+    `;
+ }
+ 
